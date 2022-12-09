3ec868b3a6bf137929ca35a3a2ca07c9ee6101c4
LWHITE <LWHITE@HBS-LWHITE>
2022-12-08 18:05:31 -0600

    updated model

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MvcMovie.Models
 {
@@ -10,7 +12,10 @@ namespace MvcMovie.Models
         [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+        
         public string? Genre { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
     }
 }
